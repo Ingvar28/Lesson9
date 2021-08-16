bcd4690f1402f44ce37c6b583fe09d97b5502885
Игорь Носов <NosovID@gmail.com>
2021-08-16 17:34:43 +0300

    Added exceptions check

--- src/ru/nosov/DoubleArray.java
+++ src/ru/nosov/DoubleArray.java
@@ -1,87 +1,116 @@
 package ru.nosov;
 
-
-import java.sql.Array;
 import java.util.Random;
 
 
 public  class DoubleArray {
-    private Random random = new Random();
+    private final Random random = new Random();
 
     //1. Написать метод для поиска наибольшего элемента в двумерном массиве.
-    public int findMaxInDoubleArray (int[][] array)  {
+    public int findMaxInDoubleArray (int[][] array)  throws ArrayIndexOutOfBoundsException, NullPointerException {
 
-        int max = array[0][0];
+        int max = 0;
 
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                if (array[i][j] > max) {
-                    max = array[i][j];
+        try{
+            for (int[] ints : array) {
+                for (int anInt : ints) {
+                    if (anInt > max) {
+                        max = anInt;
+                    }
                 }
+
             }
 
+        }catch (ArrayIndexOutOfBoundsException | NullPointerException exceptionForAnEmptyArray) {
+            System.out.println("An array need to be filled");
         }
         return max;
     }
 
     //2. Написать метод, который проверяет, является ли двумерный массив квадратным.
-    public boolean checkSquareArray (int[][] array) {
+    public boolean checkSquareArray (int[][] array)  throws ArrayIndexOutOfBoundsException, NullPointerException  {
         boolean flag = true;
 
-        for (int i = 0; i <array.length ; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                if (array.length != array[i].length){
-                    flag = false;
-                    break;
+        try {
+            for (int[] ints : array) {
+                for (int j = 0; j < ints.length; j++) {
+                    if (array.length != ints.length) {
+                        flag = false;
+                        break;
+                    }
                 }
             }
+        } catch (ArrayIndexOutOfBoundsException | NullPointerException exceptionForAnEmptyArray) {
+            System.out.println("An array need to be filled");
         }
         return flag;
     }
 
     //3. Написать метод, который, в двумерном массиве (матрице) ищет строку, сумма
     //элементов которой является максимальной среди всех строк матрицы.
-    public int findMaxRowSummary (int[][] array) {
+    public int findMaxRowSummary (int[][] array)  throws ArrayIndexOutOfBoundsException, NullPointerException  {
         double total = 0;
         double temp;
         int maxIndex = 0;
 
-        for (int i = 0; i <array.length ; i++) {
-            temp = 0;
+        try {
+            for (int i = 0; i <array.length ; i++) {
+                temp = 0;
 
-            for (int j = 0; j < array[i].length; j++) {
-                temp += array[i][j];
-            }
+                for (int j = 0; j < array[i].length; j++) {
+                    temp += array[i][j];
+                }
 
-            if (temp > total){
-                total = temp;
-                maxIndex = i;
+                if (temp > total){
+                    total = temp;
+                    maxIndex = i;
+                }
             }
+        } catch (ArrayIndexOutOfBoundsException | NullPointerException exceptionForAnEmptyArray) {
+            System.out.println("An array need to be filled");
         }
         return maxIndex;
     }
 
+
+
     //4. Двумерный массив MxN заполнить случайными символами алфавита
 
     public char[][] fillDoubleArrayByRandomValue (int m, int n) {
-        char[][] array = new char[m][n];
-        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-        for (int i = 0; i <array.length ; i++) {
-            for (int j = 0; j < array[i].length; j++) {
+        char[][] array = new char[0][];
 
-                array[i][j] =alphabet.charAt(random.nextInt(array.length)) ;
+        try {
+            if(m == 0 && n == 0 || m < 0 || n < 0)
+                throw new Exception("An array size should be more  then zero");
+
+            array = new char[m][n];
+            String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+            for (int i = 0; i <array.length ; i++) {
+                for (int j = 0; j < array[i].length; j++) {
+                    array[i][j] =alphabet.charAt(random.nextInt(alphabet.length())) ;
+                }
             }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
         return array;
     }
 
-    public void printDoubleCharArray(char[][] array) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                System.out.print(array[i][j]);
+
+
+    //Print for Double Array
+    public void printDoubleCharArray(char[][] array) throws ArrayIndexOutOfBoundsException, NullPointerException{
+        try {
+            for (char[] chars : array) {
+                for (int j = 0; j < chars.length; j++) {
+                    System.out.print(chars[j]);
+                }
+                System.out.println("");
             }
-            System.out.println("");
+        } catch (ArrayIndexOutOfBoundsException | NullPointerException exceptionForAnEmptyArray) {
+            System.out.println("An array need to be filled");
         }
     }
 
--- src/ru/nosov/Main.java
+++ src/ru/nosov/Main.java
@@ -1,32 +1,53 @@
 package ru.nosov;
 
-import java.util.Random;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         //Реализация в классе DoubleArray
 
         DoubleArray doubleArray = new DoubleArray();
 
         int[][] array = {{3, 2, 1}, {32, 1, 2}, {2, 1, 2}};
-        int[][] emptyArray = {{}, {}};
 
 
-        System.out.println("Exceptions Home Tasks");
-        System.out.println("Наибольший эелемент в массиве: "
+        System.out.println("Exceptions Home Tasks" + "\n");
+
+        //Task1
+        System.out.println("Task1");
+        System.out.println("Max element in the array: "
                 +  doubleArray.findMaxInDoubleArray(array) + "\n");
 
-        System.out.println("Является ли массив квадратным: "
+        System.out.println("Max element in the array: "
+                +  doubleArray.findMaxInDoubleArray(null) + "\n");
+
+        //Task2
+        System.out.println("Task2");
+        System.out.println("Is the array square:"
                 + doubleArray.checkSquareArray(array) + "\n");
 
-        System.out.println("Строка с максимальной суммой элементов: "
+        System.out.println("Is the array square:"
+                + doubleArray.checkSquareArray(null) + "\n");
+        //Task3
+        System.out.println("Task3");
+        System.out.println("the string with the maximum sum of elements:"
                 + doubleArray.findMaxRowSummary(array) + "\n");
 
-        System.out.println("Массив со случайными значениями: ");
+        System.out.println("the string with the maximum sum of elements:"
+                + doubleArray.findMaxRowSummary(null) + "\n");
+
+        //Task4
+        System.out.println("Task4");
+        System.out.println("Array with random literal values:");
         doubleArray.printDoubleCharArray
-                (doubleArray.fillDoubleArrayByRandomValue(5, 3));
+                (doubleArray.fillDoubleArrayByRandomValue(3, 8));
+
+        doubleArray.printDoubleCharArray
+                (doubleArray.fillDoubleArrayByRandomValue(0, 0));
+
+        doubleArray.printDoubleCharArray
+                (doubleArray.fillDoubleArrayByRandomValue(-1, -1));
 
     }
 
